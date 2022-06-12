d2749f01acdff0993651e688f2a32c6323e2d878
Aleksey Shaydullin <production@blackbirdradio.ru>
2022-06-12 22:48:59 +0300

    Прописать логику открытия и закрытия Бургер меню

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 //DOM
-const burger = document.querySelector('.navigation__list');
+const burger = document.querySelector('.header__burger');
+const modalBurger = document.querySelector('.modal-burger');
+const moadalNovigation = document.querySelector('#modal-navigation');
+const modalSwitcher = document.querySelector('#modal-switcher')
+const navigationVariety = document.querySelector('#navigation-variety');
+const navigationChoiceBike = document.querySelector('#navigation-choice-bike');
+const navigationWorkout = document.querySelector('#navigation-workout');
 
 //Бургер меню
-function burgerMenu() {
-  burger.addEventListener('click', () => {
-    burger.classList.toggle('navigation__list_activ')
-  })
+
+//Открытие Бургер меню
+
+function openCloseMenu() {
+  burger.classList.toggle('header__burger_activ');
+  modalBurger.classList.toggle('modal-burger_activ');
+  moadalNovigation.classList.toggle('navigation_activ');
+  modalSwitcher.classList.toggle('footer-switcher_active');
 }
+
+function burgerMenu() {
+  burger.addEventListener('click', (evt) => openCloseMenu());
+}
+
+burgerMenu();
+
+//Закрытие Бургер меню
+navigationVariety.addEventListener('click', (evt) => openCloseMenu());
+
+moadalNovigation.addEventListener('click', (evt) => openCloseMenu());
+
+navigationVariety.addEventListener('click', (evt) => openCloseMenu());
