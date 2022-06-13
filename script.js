bcf0421df8ba6fc08f60121fe7c8f1763b0a247b
Aleksey Shaydullin <production@blackbirdradio.ru>
2022-06-13 03:08:10 +0300

    сделать тёмную тему сайта

--- script.js
+++ script.js
@@ -6,7 +6,22 @@ const modalSwitcher = document.querySelector('#modal-switcher')
 const navigationVariety = document.querySelector('#navigation-variety');
 const navigationChoiceBike = document.querySelector('#navigation-choice-bike');
 const navigationWorkout = document.querySelector('#navigation-workout');
-const switcherLight = document.querySelector('.footer-switcher__light');
+const switcherList = document.querySelectorAll('.footer-switcher__light');
+const page = document.querySelector('.page');
+const header = document.querySelector('.header');
+const navLinkList = document.querySelectorAll('.navigation__link');
+const leadSubtitle = document.querySelector('.lead__subtitle');
+const autorSubline = document.querySelector('.signature__quote-author-subline');
+const varietySubtitle = document.querySelector('.variety__subtitle');
+const rowButtonList = document.querySelectorAll('.row-buttons__button');
+const arrowLeft = document.querySelector('.row-buttons__button_left');
+const arrowRight = document.querySelector('.row-buttons__button_right');
+const bikeMenuList = document.querySelectorAll('.choice-bike-menu__link');
+const bikeMenuActiv = document.querySelector('.choice-bike-menu__link_activ');
+const workoutSubtitle = document.querySelector('.workout__subtitle');
+const workoutLinkList = document.querySelectorAll('.workout-links__link');
+const footerInput = document.querySelector('.footer__input');
+const footerCopyright = document.querySelector('.footer__copyright');
 
 //Бургер меню
 
@@ -35,9 +50,26 @@ navigationVariety.addEventListener('click', (evt) => openCloseMenu());
 //Темная тема сайта 
 
 function darkTheme() {
-  switcherLight.addEventListener('click', () => {
-    switcherLight.classList.toggle('footer-switcher__light_dark');
-  })
+  switcherList.forEach(switcher => switcher.addEventListener('click', function (evt) {
+    switcher.classList.toggle('footer-switcher__light_dark');
+    page.classList.toggle('page_dark');
+    header.classList.toggle('header_dark');
+    navLinkList.forEach(navLink => navLink.classList.toggle('navigation__link_dark'));
+    leadSubtitle.classList.toggle('lead__subtitle_dark');
+    autorSubline.classList.toggle('signature__quote-author-subline_dark');
+    varietySubtitle.classList.toggle('variety__subtitle_dark');
+    rowButtonList.forEach(rowButton => rowButton.classList.toggle('row-buttons__button_dark'));
+    arrowLeft.classList.toggle('row-buttons__button_left-dark');
+    arrowRight.classList.toggle('row-buttons__button_right-dark');
+    bikeMenuList.forEach(bikeMenu => bikeMenu.classList.toggle('choice-bike-menu__link_dark'));
+    bikeMenuActiv.classList.toggle('choice-bike-menu__link_activ-dark');
+    workoutSubtitle.classList.toggle('workout__subtitle_dark');
+    workoutLinkList.forEach(workoutLink => workoutLink.classList.toggle('workout-links__link_dark'));
+    footerInput.classList.toggle('footer__input_dark');
+    footerCopyright.classList.toggle('footer__copyright_dark');
+    modalBurger.classList.toggle('modal-burger_dark');
+  }));
 }
+
 
 darkTheme();
